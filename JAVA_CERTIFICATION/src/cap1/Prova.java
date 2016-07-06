13ccb9e27ed6235ec7ec9ecd76e0b8c858f8bfae
liliana Poveda <poveda.liliana18@gmail.com>
2016-07-07 00:52:13 +0200

    First change

--- JAVA_CERTIFICATION/src/cap1/Prova.java
+++ JAVA_CERTIFICATION/src/cap1/Prova.java
@@ -1,9 +1,9 @@
 package cap1;
 
 public class Prova {
-	String name = "Liliana";
+	String name = "ProvaGit";
 	public static void main(String[] args) {
-		System.out.println("Lilly");
+		System.out.println("ProvaGit");
 
 	}
 
